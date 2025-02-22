a5a0b8a9421d4b8545bb6f2837cba24766e93436
Shreya Acharya <vsa.shreya@gmail.com>
2025-02-22 11:05:48 +0530

    Fix build issue

---
+++ client-marks-predictor/build/precache-manifest.420507987cae17bb7bc57f11d2c1f33b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b44b4edc3b8437507cac7c5e1a899ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa1c7927c52cb07f5991",
+    "url": "/static/css/main.c48c7e2a.chunk.css"
+  },
+  {
+    "revision": "2e73cb40493bbbec0142",
+    "url": "/static/js/2.3ebced83.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.3ebced83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa1c7927c52cb07f5991",
+    "url": "/static/js/main.8a0175be.chunk.js"
+  },
+  {
+    "revision": "d7bbcd76cac6a48579de",
+    "url": "/static/js/runtime-main.154c8d62.js"
+  }
+]);
